c9deb9664b6963075e616ff8b203b8c65cd26de4
NyakalloLegong <nyakallolegong@gmail.com>
2023-02-16 08:02:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always o
+ * Return: o (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (o);
+puts("\"Programming is like building a multilingual puzzle");
+return (o);
 }
-
