9be19cd310787911d569ba63429aa5ed119fe0bd
NyakalloLegong <nyakallolegong@gmail.com>
2023-02-22 14:17:32 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,10 @@
 
 /**
  * print_sign - prints the sign of a number
+ * greater, iqual or less than zero
+ *
  * @n: the int to check
+ *
  * Return: 1 and prints + if n is greater than zero
  * -1 and prints - if n is less than zero
  */
@@ -10,15 +13,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	} else if (n == 0)
-}	
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
 		_putchar(48);
 		return (0);
-	} else if (n < 0)
-{
-	_putchar('-');
-}
-	return (-1);
+	}
+	_putchar('\n');
 }
