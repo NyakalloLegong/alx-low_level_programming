c89157735299c41fe196880335ca48e42f59bc71
NyakalloLegong <nyakallolegong@gmail.com>
2023-03-01 23:24:29 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,5 +7,22 @@
  *
  * Return: dest
  */
-char *_strncpy(char *dest, char
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
 
